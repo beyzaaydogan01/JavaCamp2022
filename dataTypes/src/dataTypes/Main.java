0dafead9b909902210c58db2b7653e40bc82bc06
beyzaaydogan01 <113782704+beyzaaydogan01@users.noreply.github.com>
2022-09-19 23:28:42 +0300

    Java kamp
    hafta 1 ödev 2

---
+++ dataTypes/src/dataTypes/Main.java
@@ -0,0 +1,16 @@
+package dataTypes;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int sayi = 12;
+
+		double sayi1 = 12.5;
+
+		String sehir = "ANKARA";
+		char karakter = 'A';
+		
+		boolean dogruMu = true;
+		
+	}
+}
---
+++ veriables/src/veriables/Main.java
@@ -0,0 +1,10 @@
+package veriables;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int ogrenciSayisi = 12;
+		String mesaj = "Öğrenci sayısı : ";
+		System.out.println(mesaj + ogrenciSayisi);
+	}
+}
